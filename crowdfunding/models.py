344f5509dcb48a468d7d28e9bf8c79aca2460fb4
Akshat Arora <akshatarora826@gmail.com>
2017-12-14 22:16:07 +0000

    Update models.py

--- crowdfunding/models.py
+++ crowdfunding/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
-#from django.urls import reverse 
 from django.db import models
-from core.models import User
+from core.models import User,UnivAlum
 
 import uuid
 import os
@@ -10,54 +9,46 @@ import os
 
 
 def get_image_path(instance, filename):
-    return os.path.join('photos', str(instance.id), filename)
+    name, ext = os.path.splitext(filename)
+    return '{0}/{1}'.format("projectimages", str(instance.title)+str(ext))
 
 
+def get_file_path(instance, filename):
+    name, ext = os.path.splitext(filename)
+    return '{0}/{1}'.format("projecttimeline", str(instance.title)+str(ext))
 
-class Alum_Fund(models.Model):
-    first_name = models.CharField(max_length=100,verbose_name="First Name")
-    last_name = models.CharField(max_length=100,verbose_name="Last Name")
+
+class AlumFund(models.Model):
+    name = models.CharField(max_length=100,verbose_name="Alumni Name")
     funds = models.IntegerField(verbose_name="Funds Given")
     def __str__(self):
-        return '%s, %s, %s' % (self.first_name, self.last_name,self.funds)
+        return '%s' % (self.name)
 
     class Meta:
-        ordering = ["first_name"]
-        
-class Alum_Votes(models.Model):
-    first_name = models.CharField(max_length=100,verbose_name="First Name")
-    last_name = models.CharField(max_length=100,verbose_name="Last Name")
-    votes = models.IntegerField(verbose_name="Votes Given")
-
-    def __str__(self):
-        return '%s, %s, %s' % (self.first_name, self.last_name,self.votes)
-
-    class Meta:
-        ordering = ["first_name"]        
+        ordering = ["name"]
         
 
-class Students_Project(models.Model):
+
+class StudentsProject(models.Model):
     """
     Model representing a Student_project
     """
     title = models.CharField(max_length=200,verbose_name="Title")
-    student = models.ManyToManyField(User,verbose_name="Student",related_name="Student User+")
-    alumni = models.ManyToManyField(User,verbose_name="Alumni",related_name="Alumni User+")
-    votes = models.IntegerField(verbose_name="Votes")
-    timeline = models.CharField(max_length=200,verbose_name="Timeline")
+    student = models.ManyToManyField(User,verbose_name="Student",related_name="StudentUser+",null=True, blank=True)
+    alumni = models.ForeignKey(User,verbose_name="Alumni",related_name="AlumniUser+",null=True, blank=True)
+    timeline = models.FileField(upload_to=get_file_path)
     project_image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
     funds = models.IntegerField(verbose_name="Allocated Amount ")
-    raised_amount =  models.IntegerField(verbose_name="Raised Amount")
     description = models.TextField(max_length=1000,verbose_name="Description")
-    alum_funds = models.ManyToManyField(Alum_Fund,help_text="Alumni who funded for this project",verbose_name="Funds Alumni")
-    alum_votes = models.ManyToManyField(Alum_Votes,help_text="Alumni who voted for this project",verbose_name="Votes Alumni")    
+    alum_funds = models.ManyToManyField(AlumFund,help_text="Alumni who funded for this project",verbose_name="Funds Alumni",related_name="AlumFund",null=True, blank=True)
+    alum_votes = models.ManyToManyField(User,help_text="Alumni who voted for this project",verbose_name="Votes Alumni",related_name="AlumVote+",null=True, blank=True)    
 
    
     def __str__(self):
         """
         String for representing the Model object.
         """
-        return '%s, %s, %s, %s, %s, %s' % (self.title, self.votes, self.project_image, self.funds, self.votes,self.description)
+        return '%s' % (self.title)
     
     class Meta:
         ordering = ["title"]    
